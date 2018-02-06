9f0c2ef5093684fc9428b279fea7873325f51a39
websterbei <bei.yijie.2014@vjc.sg>
2018-02-05 20:47:18 -0500

    stage 0 half done

--- app/src/main/java/edu/duke/quizmaster/MainActivity.java
+++ app/src/main/java/edu/duke/quizmaster/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.duke.quizmaster;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startQuizButton = findViewById(R.id.startQuizButton);
+        startQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openQuiz(view.getContext());
+            }
+
+            private void openQuiz(Context mContext) {
+                Intent intent = new Intent(mContext, QuizActivity.class);
+                intent.putExtra("quizProblemName", "quiz1");
+                mContext.startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/edu/duke/quizmaster/QuizActivity.java
+++ app/src/main/java/edu/duke/quizmaster/QuizActivity.java
@@ -2,6 +2,11 @@ package edu.duke.quizmaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        TextView quizProblem = findViewById(R.id.quizProblem);
+        String quizProblemName = getIntent().getExtras().getString("quizProblemName");
+        int quizID = getResources().getIdentifier(quizProblemName, "array", getPackageName());
+        final String[] quizProblemArray = getResources().getStringArray(quizID);
+        quizProblem.setText(quizProblemArray[0]);
+        RadioButton optionA = findViewById(R.id.optionA);
+        RadioButton optionB = findViewById(R.id.optionB);
+        RadioButton optionC = findViewById(R.id.optionC);
+        RadioButton optionD = findViewById(R.id.optionD);
+        optionA.setText(quizProblemArray[1]);
+        optionB.setText(quizProblemArray[2]);
+        optionC.setText(quizProblemArray[3]);
+        optionD.setText(quizProblemArray[4]);
+
+        Button submissionButton = findViewById(R.id.submit);
+        submissionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioGroup radioGroup = findViewById(R.id.radioGroup);
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton selectedButton = (RadioButton) findViewById(selectedId);
+                String selectedAnswer = selectedButton.getText().toString();
+                int score = selectedAnswer.equals(quizProblemArray[5]) ? 1:0;
+                
+            }
+        });
     }
 }
