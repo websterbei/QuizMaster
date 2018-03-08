e42b44f39632c4f30ad1454e18b90d892b0ba2df
websterbei <bei.yijie.2014@vjc.sg>
2018-03-08 14:23:23 -0500

    added unit test

--- app/src/test/java/edu/duke/quizmaster/PersonalityQuestionClassTest.java
+++ app/src/test/java/edu/duke/quizmaster/PersonalityQuestionClassTest.java
@@ -12,21 +12,7 @@ import static org.junit.Assert.assertEquals;
  * Created by Webster on 3/6/18.
  */
 
-public class QuestionClassTest {
-    @Test
-    public void testLinearQuestion() {
-        String query = "What is 1+1?";
-        ArrayList<String> answers = new ArrayList<>(Arrays.asList("1", "2", "3", "4"));
-        ArrayList<Integer> scores = new ArrayList<>(Arrays.asList(0,1,0,0));
-        LinearQuestion q = new LinearQuestion(query, answers, scores);
-        assertEquals(q.getScore("1"), 0);
-        assertEquals(q.getScore("2"), 1);
-        assertEquals(q.getScore("3"), 0);
-        assertEquals(q.getScore("4"), 0);
-
-        assertEquals(q.getQuery(), "What is 1+1?");
-    }
-
+public class PersonalityQuestionClassTest {
     @Test
     public void testPersonalityQuestion() {
         String query = "What is 1+1?";
